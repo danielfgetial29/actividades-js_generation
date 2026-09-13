1ba64d35aab55113ba7d74766a10b86000d15827
Daniel Getial <danielg.soft29@gmail.com>
2026-09-13 00:46:37 -0500

    feat: carpeta para ejercicios, con los ejercicios de la guia

---
+++ clase_11_sept/variables-tipos-de-datos/ejercicios/ejercicio1.js
@@ -0,0 +1,13 @@
+console.log("=== EJERCICIO 1 ===");
+
+let nombre = "Daniel Getial";
+let edad = 25;
+let esEstudiante = true;
+let ciudad = "Cali";
+let hobby = "Jugar futbol";
+
+console.log(`Mi nombre es ${nombre}`);
+console.log(`Tengo ${25} años`);
+console.log(`Soy estudiante de Generation? ${esEstudiante}`);
+console.log(`Vivo en ${ciudad}`);
+console.log(`Mi hobby es ${hobby}`);
---
+++ clase_11_sept/variables-tipos-de-datos/ejercicios/ejercicio2.js
@@ -0,0 +1,1 @@
+//Practicar operadores aritméticos.
