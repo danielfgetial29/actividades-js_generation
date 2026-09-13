fa4479a56720d5940ef097078e7244031e9dd105
Daniel Getial <danielg.soft29@gmail.com>
2026-09-13 00:20:44 -0500

    primeros registros en varaibles.js sobre let,const y operadores aritmeticos

---
+++ clase_11_sept/variables-tipos-de-datos/variables.js
@@ -0,0 +1,31 @@
+// Definiciones de conceptos fundamentales en variables, tipos de datos
+// y estructuras de datos en JavaScript
+
+// Variables
+// let para asignar variables que van a cambiar
+let nombre = "daniel";
+console.log(nombre);
+
+let edad = 25;
+console.log(edad);
+
+//const cuando el valor no cambiaria y sera una CONSTANTE
+const PI = 3.416;
+console.log(PI);
+
+// const PI = 4;
+// console.log(PI);
+
+// operadores aritmeticos
+let suma = 10 + 5;
+let resta = 10 - 5;
+// 15
+// 5
+let multiplicacion = 10 * 5; // 50
+let division = 10 / 5;
+// 2
+let residuo = 10 % 3;
+let potencia = 2 ** 3;
+// 1 - Módulo (residuo de la división)
+// 8 - Potencia (2 elevado a 3)
+console.log("El resultado de la división es: " + division);
