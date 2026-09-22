014a93f628890219513f73166068ab0e91061c75
Daniel Getial <danielg.soft29@gmail.com>
2026-09-22 00:14:30 -0500

    Se realiza la tarea 5 que es pedir un limite y analizar un numero en especifico

--- ejercicios/sd-l1-1.0/task5.js
+++ ejercicios/sd-l1-1.0/task5.js
@@ -1,24 +1,55 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
 // Consulta la Tarea 5 en tus Instrucciones para completar esta tarea
-let rango = Number(prompt("Hasta que numero quieres analizar: "));
+console.log("=== Desafio FizzBuzz ===\n");
 
-for (let i = 0; i < rango + 1; i++) {
-  if (i % 3 === 0 && i % 5 === 0 && i % 7 === 0) {
-    console.log("FizzBuzzWoof");
-  } else if (i % 3 === 0 && i % 7 === 0) {
-    console.log("FizzWoof");
-  } else if (i % 5 === 0 && i % 7 === 0) {
-    console.log("BuzzWoof");
-  } else if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
-  } else if (i % 7 === 0) {
-    console.log("Woof");
+console.log("1. Indicar limite");
+console.log("2. Evaluar un solo valor");
+
+const decision = [1, 2];
+
+while (true) {
+  let respuesta = Number(prompt("Ingresa el numero de la opcion a elegir: "));
+
+  if (decision.includes(respuesta)) {
+    if (respuesta === 1) {
+      let rango = Number(prompt("Hasta qué número quieres analizar: "));
+      fizzBuzz(rango);
+      break;
+    } else if (respuesta === 2) {
+      let numero = Number(prompt("Ingresa un número a analizar: "));
+      let resultado = oneNumber(numero);
+      console.log(resultado);
+      break;
+    }
   } else {
-    console.log(i);
+    console.log("Opción no válida. Por favor elige 1 o 2.");
+  }
+}
+
+function oneNumber(number) {
+  if (number % 3 === 0 && number % 5 === 0 && number % 7 === 0) {
+    return "FizzBuzzWoof";
+  } else if (number % 3 === 0 && number % 7 === 0) {
+    return "FizzWoof";
+  } else if (number % 5 === 0 && number % 7 === 0) {
+    return "BuzzWoof";
+  } else if (number % 3 === 0 && number % 5 === 0) {
+    return "FizzBuzz";
+  } else if (number % 3 === 0) {
+    return "Fizz";
+  } else if (number % 5 === 0) {
+    return "Buzz";
+  } else if (number % 7 === 0) {
+    return "Woof";
+  } else {
+    return number;
+  }
+}
+
+function fizzBuzz(limite) {
+  for (let i = 1; i < limite + 1; i++) {
+    let resultado = oneNumber(i);
+    console.log(resultado);
   }
 }
