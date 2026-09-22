ec8a450a32673cf74d236597fa4c19e77233bb9e
Daniel Getial <danielg.soft29@gmail.com>
2026-09-21 23:36:27 -0500

    Se agrega limite por consola

--- ejercicios/sd-l1-1.0/task5.js
+++ ejercicios/sd-l1-1.0/task5.js
@@ -1,5 +1,24 @@
-// Consulta la Tarea 5 en tus Instrucciones para completar esta tarea
+const prompt = require("prompt-sync")({ sigint: true });
 
-for (let i = 0; i < 1; i++) {
-  console.log("¡Esta es la Tarea Cinco!");
-};
+// Consulta la Tarea 5 en tus Instrucciones para completar esta tarea
+let rango = Number(prompt("Hasta que numero quieres analizar: "));
+
+for (let i = 0; i < rango + 1; i++) {
+  if (i % 3 === 0 && i % 5 === 0 && i % 7 === 0) {
+    console.log("FizzBuzzWoof");
+  } else if (i % 3 === 0 && i % 7 === 0) {
+    console.log("FizzWoof");
+  } else if (i % 5 === 0 && i % 7 === 0) {
+    console.log("BuzzWoof");
+  } else if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else if (i % 7 === 0) {
+    console.log("Woof");
+  } else {
+    console.log(i);
+  }
+}
