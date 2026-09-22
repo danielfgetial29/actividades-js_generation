ee1cf8a454b61fdbcb7bdbb1951da2da8d338995
Daniel Getial <danielg.soft29@gmail.com>
2026-09-22 11:57:58 -0500

    Task2, utilizando .reverse()

--- ejercicios/objetos-arreglos/task1.js
+++ ejercicios/objetos-arreglos/task1.js
@@ -1,6 +1,11 @@
 const arr = [];
 
 // ¡Escribe tu código debajo de esta línea!
+//uso for para llenar el array, declaro 1 en 1 que es donde empieza
+// luego la condicion para que se detenga que es que i debe ser menor a 21
+// y el contador o el incremento para que aumente de 1 en 1,
+// y en cada recorrido va guardando el incremento con push()
+
 for (let i = 1; i < 21; i++) {
   arr.push(i);
 }
