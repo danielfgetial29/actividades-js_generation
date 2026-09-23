6a5a7cff8605a5e25dd2ed880c3c318e9834eeb9
Daniel Getial <danielg.soft29@gmail.com>
2026-09-22 23:57:46 -0500

    solucion task7, operaciones en matrices. Insertar un valor en una fila

--- ejercicios/objetos-arreglos/task7.js
+++ ejercicios/objetos-arreglos/task7.js
@@ -1,11 +1,14 @@
 const arr = [
-    [0,1,2,3,4,5,6,7,8,9],
-    [10,11,12,13,14,15,16,17,18,19],
-    [20,21,22,23,24,25,26,27,28,29]
-  ]
-  
-  // ¡Escribe tu código debajo de esta línea!
-  
-  
-  
-  // ¡Escribe tu código encima de esta línea!
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+  [20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
+];
+
+// ¡Escribe tu código debajo de esta línea!
+
+//Agregar un numero en la matriz con .push()
+// arr[0] esto es la primera fila de la matriz y con push lo agrega al final
+arr[0].push(100);
+console.log("Primera fila de la matriz [0]", arr[0]);
+
+// ¡Escribe tu código encima de esta línea!
