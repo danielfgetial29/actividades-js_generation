6566c63b63742195c0ef61914e1c1642dd3eacd2
Daniel Getial <danielg.soft29@gmail.com>
2026-09-23 00:09:39 -0500

    Borrar ultimo elemento en una matriz y agregar una fila completa

--- ejercicios/objetos-arreglos/task7.js
+++ ejercicios/objetos-arreglos/task7.js
@@ -11,4 +11,12 @@ const arr = [
 arr[0].push(100);
 console.log("Primera fila de la matriz [0]", arr[0]);
 
+//Para eliminar el ultimo elemento de la primera fila
+arr[0].pop();
+console.log(`Columnas de la primera fila ${arr[0].length}`);
+
+// Agregar una fila completa de numeros
+arr.push([30, 31, 32, 33, 34, 35, 36, 37, 38, 39]);
+console.log(`Filas: ${arr.length} y columnas: ${arr[3].length}`);
+
 // ¡Escribe tu código encima de esta línea!
