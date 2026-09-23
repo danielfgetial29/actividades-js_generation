abf4dabb0806c24b3b5fa81ee8536cd081f3a141
Daniel Getial <danielg.soft29@gmail.com>
2026-09-23 16:33:51 -0500

    task2, recibir datos por consola con process.argv[]

--- ejercicios/lb-propiedades-metodos/task2.js
+++ ejercicios/lb-propiedades-metodos/task2.js
@@ -1,12 +1,14 @@
 function Mail(asunto, mensaje) {
-  this.asunto = asunto
-  this.mensaje = mensaje
+  this.asunto = asunto;
+  this.mensaje = mensaje;
 }
 
 // ¡Escribe tu código debajo de esta línea!
+let asunto = process.argv[3];
+let mensaje = process.argv[4];
 
-const nuevoCorreo = new Mail()
+const nuevoCorreo = new Mail(asunto, mensaje);
 
 // ¡Escribe tu código encima de esta línea!
 
-console.log(nuevoCorreo.asunto + ": " + nuevoCorreo.mensaje)
+console.log(nuevoCorreo.asunto + ": " + nuevoCorreo.mensaje);
