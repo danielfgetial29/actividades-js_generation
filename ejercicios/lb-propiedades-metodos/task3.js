21cccd47a54f7d06111b01607a247bc8a496ee7b
Daniel Getial <danielg.soft29@gmail.com>
2026-09-23 16:58:44 -0500

    solucion task3 usando un funcion en el cosntructor

--- ejercicios/lb-propiedades-metodos/task3.js
+++ ejercicios/lb-propiedades-metodos/task3.js
@@ -1,13 +1,18 @@
 // ¡Escribe tu código debajo de esta línea!
 
 function Mail(asunto, mensaje) {
-  this.asunto = asunto
-  this.mensaje = mensaje
-
+  this.asunto = asunto;
+  this.mensaje = mensaje;
+  this.imprimirCorreo = function () {
+    console.log(`${this.asunto}: ${this.mensaje}`);
+  };
 }
 
-const nuevoCorreo = new Mail()
+let entrada1 = process.argv[3];
+let entrada2 = process.argv[4];
+
+const nuevoCorreo = new Mail(entrada1, entrada2);
 
 // ¡Escribe tu código encima de esta línea!
 
-nuevoCorreo.imprimirCorreo()
+nuevoCorreo.imprimirCorreo();
