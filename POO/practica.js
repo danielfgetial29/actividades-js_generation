52fcac85991b84cd4b0104162d6bf91f27d6de51
Daniel Getial <danielg.soft29@gmail.com>
2026-09-24 12:15:00 -0500

    uso de Object.create() para crear una instancia nueva apartir de otra instancia ya existente

--- POO/practica.js
+++ POO/practica.js
@@ -1,4 +1,4 @@
-const ListaPeliculas = [];
+const listaPeliculas = [];
 
 function Pelicula(nombre, vista, duracion) {
   this.nombrePelicula = String(nombre); // convierte todo lo que ingrese en String
@@ -22,5 +22,32 @@ pelicula1.mostrarInfo(); // realizar funcion de mostrar informacion de la pelicu
 const pelicula2 = new Pelicula("La odiesea", true, 100);
 const pelicula3 = new Pelicula("La monja", false, 90);
 
-ListaPeliculas.push(pelicula1, pelicula2, pelicula3);
-console.log(ListaPeliculas);
+// agregamos las instancias a el array que teniamos en el principio del codigo
+listaPeliculas.push(pelicula1, pelicula2, pelicula3);
+console.log(listaPeliculas);
+
+console.log(pelicula1.nombrePelicula);
+
+// para ver todas las peliculas
+// console.log("Toda la lista de peliculas");
+// for (let pelicula of listaPeliculas) pelicula.mostrarInfo();
+
+// console.log(listaPeliculas[0].slice(0, 2));
+
+// creamos una instancia nueva apartir de una ya existente
+const pelicula4 = Object.create(pelicula1);
+pelicula4.mostrarInfo();
+
+pelicula4.nombrePelicula = "Titanic";
+pelicula4.duracionPelicula = 250;
+pelicula4.vistaPelicula = true;
+
+//agregamos la nueva pelicula
+listaPeliculas.push(pelicula4);
+
+// mostramos nuevamente todos los datos en
+// listaPeliculas con su index
+for (let pelicula of listaPeliculas) {
+  console.log(listaPeliculas.indexOf(pelicula));
+  pelicula.mostrarInfo();
+}
