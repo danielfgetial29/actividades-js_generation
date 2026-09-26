3ea8ecdf8cf1c3062950283732f8f2932c3d9dbd
Daniel Getial <danielg.soft29@gmail.com>
2026-09-26 01:41:14 -0500

    tas2. Se realiza de dos maneras, usando el index.js y usando el mismo archivo task2.js, esto para practicar la logica unicamente en el archivo

--- laboratorios/lb-15-modulos-dependencias/task2.js
+++ laboratorios/lb-15-modulos-dependencias/task2.js
@@ -1,3 +1,22 @@
+// variables (en este caso no las usamos ya que el index.js las obtiene)
+let nombreAmigo1 = process.argv[3];
+let nombreAmigo2 = process.argv[4];
+let nombreAmigo3 = process.argv[5];
+
+//
+
 export class NombresAmigos {
-  constructor() {}
+  constructor(nombre1, nombre2, nombre3) {
+    this.nombre1 = nombre1;
+    this.nombre2 = nombre2;
+    this.nombre3 = nombre3;
+  }
+  agregarAmigos() {
+    let nombresObtenidos = [this.nombre1, this.nombre2, this.nombre3];
+    return nombresObtenidos;
+  }
 }
+
+// instanciamos la clase
+const amigos = new NombresAmigos(nombreAmigo1, nombreAmigo2, nombreAmigo3);
+console.log(amigos.agregarAmigos());
