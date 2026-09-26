47b52adb1624e5195a9117bad8e53a318a6efb42
Daniel Getial <danielg.soft29@gmail.com>
2026-09-25 23:03:24 -0500

    Solucion task2 - mostrar propiedad nivel

--- laboratorios/lb-14-herencia-clases/task2.js
+++ laboratorios/lb-14-herencia-clases/task2.js
@@ -1,7 +1,15 @@
 export class Jugador {
-  constructor() {
-
+  constructor(nombre, nivel) {
+    this.nombre = String(nombre);
+    this.nivel = Number(nivel);
   }
-
-
 }
+
+// variables con argumentos a recibir
+let player_name = process.argv[2];
+let player_level = process.argv[3];
+
+// instancia de Jugador()
+const jugador1 = new Jugador(player_name, player_level);
+
+console.log(jugador1.nivel);
