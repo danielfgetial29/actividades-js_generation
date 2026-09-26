670b26def0ab1fa737ccc00a68b0679b60a6d9f9
Daniel Getial <danielg.soft29@gmail.com>
2026-09-25 22:00:25 -0500

    Solucion lb-14 Importar metodos y clases y agrego lb-15

--- laboratorios/lb-15-modulos-dependencias/task1.js
+++ laboratorios/lb-15-modulos-dependencias/task1.js
@@ -1,3 +1,8 @@
-export function calculadoraCosto() {
+let transancion = process.argv[2];
 
+export function calculadoraCosto(monto) {
+  const TASA_FIJA = 3;
+  let valor_a_pagar = monto * 0.01 + monto + TASA_FIJA;
+  return valor_a_pagar;
 }
+console.log(calculadoraCosto(transancion));
